a946e5e7de97616735c13deb12f526c907ebeb3c
BigUncleYemi <olayemioyebanji911@gmail.com>
2018-03-23 22:27:35 +0100

    project start

---
+++ src/component/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../css/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default App;
