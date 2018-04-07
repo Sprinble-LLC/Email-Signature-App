0c519e3502c7430a4c60826a8aeb1379fdee6484
Sprinble <36000578+sprinble@users.noreply.github.com>
2018-04-07 12:11:09 +0100

    update header component

--- src/component/Header.js
+++ src/component/Header.js
@@ -9,7 +9,7 @@ export default class Header extends Component {
                     <h1>Hi, I create <strong>Email Signatures</strong>, another easy<br />
                     way to create email signature built by <a href="http://sprinble.com">Sprinble</a>.</h1>
                     <p>The good news is that I am open source.<br />
-                    You can star and fork me on <a href="http://sprinble.com">Github</a>.</p>
+                    You can star and fork me on <a href="https://github.com/Sprinble-LLC/Email-Signature-App">Github</a>.</p>
                     <ul className="actions">
                         <li><a href="#two" className="button scrolly">Create Signature</a></li>
                     </ul>
